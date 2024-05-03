eeeacfea0ddfbc4240a52e44306f0360749608c6
samy <samyrahakbauw@protonmail.com>
2024-05-03 13:42:47 +0200

    Added description

--- home-desktop.nix
+++ home-desktop.nix
@@ -1,6 +1,9 @@
+#
+# This is just my first attempt at making a configuration
+#
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running ‘nixos-help’).A
 
 { config, pkgs, ... }:
 
